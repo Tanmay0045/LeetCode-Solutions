84e6a7b879be4038aa64e3cae6cd5be07391cfe0
Tanmay0045 <tanmay0000045@gmail.com>
2026-08-28 23:56:57 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.9%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+int insertpos = 0;
+
+for(int i=0;i<nums.size();i++){
+    if(nums[i]!=0){
+        nums[insertpos]=nums[i];
+        insertpos++;
+    }
+}
+
+for(int i=insertpos;i<nums.size();i++){
+    nums[i]=0;
+}
+
+
+
+
+
+    }
+};
