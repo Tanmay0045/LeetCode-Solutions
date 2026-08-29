cad3b1173b3624d19b5f9873c3bba459afc65167
Tanmay0045 <tanmay0000045@gmail.com>
2026-08-29 09:49:50 +0530

    Time: 0 ms (100%), Space: 24 MB (19.47%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         
-int zero = 0;
+int indexingonwhichelementiszero = 0;
 for(int i=0;i<nums.size();i++){
     if(nums[i]!=0){
-        swap(nums[zero],nums[i]);
-        zero++;
+        swap(nums[indexingonwhichelementiszero],nums[i]);
+        indexingonwhichelementiszero++;
     }
 }
 
